9e32315327b2ca92b94e341fdcf36b097eaf2992
Simge Erdil <siisi4officiel@gmail.com>
2020-09-27 01:41:01 +0200

    torche + porte

---
+++ Assets/Scripts/passageportelaby.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class passageportelaby : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Laby");
+        }
+    }
+
+}
