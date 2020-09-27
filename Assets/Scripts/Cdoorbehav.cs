4705f104aeaddcdb0d12477ed35a2c44a22f3b91
Simge Erdil <siisi4officiel@gmail.com>
2020-09-28 00:40:40 +0200

    scene C complet

---
+++ Assets/Scripts/Cdoorbehav.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class Cdoorbehav : MonoBehaviour
+{
+    public GameObject canvasbait;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canvasbait.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            SceneManager.LoadScene("Game");
+            Debug.Log("R pressed");
+
+        }
+    }
+}
