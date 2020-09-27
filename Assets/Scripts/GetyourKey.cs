2d94776c239fa2b2c0442bc6129669b789bd5332
Simge Erdil <siisi4officiel@gmail.com>
2020-09-27 16:22:46 +0200

    cle ramasser + text you've got a key

--- Assets/Scripts/GetyourKey.cs
+++ Assets/Scripts/GetyourKey.cs
@@ -1,32 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GetyourKey : MonoBehaviour
 {
+    public static int keyCount;
+    public GameObject canvas;
 
-
-    public GameObject key;
-
-    void Start()
+    private void OnTriggerEnter(Collider collider)
     {
-        
-    }
-
-    
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            key.SetActive(false);
+        if (collider.gameObject.name == "player"){
+            keyCount += 2;
+            Destroy(gameObject);
+            canvas.SetActive(true);
         }
-    } 
-    
+
+    }
+
 
 
 
