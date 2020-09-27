d3b20699e7fca161d07203b9dbc3a2617597fa29
Simge Erdil <siisi4officiel@gmail.com>
2020-09-27 20:57:53 +0200

    ghost talking + e press for skip text

---
+++ Assets/Scripts/GhostBehav.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class GhostBehav : MonoBehaviour
+{
+
+
+    public GameObject subtitle;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            subtitle.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            subtitle.SetActive(true);
+        }
+    }
+
+}
