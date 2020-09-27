db6539b9262c2bcfecc1d83bea7f6d0b98d3e921
Simge Erdil <siisi4officiel@gmail.com>
2020-09-27 03:10:30 +0200

    rammasser la torche

---
+++ Assets/Scripts/torcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torcher : MonoBehaviour
+{
+
+    public GameObject torche;
+    public GameObject torchej;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            torche.SetActive(false);
+            Debug.Log("torche triggered");
+            torchej.SetActive(true);
+        }
+    }
+
+}
