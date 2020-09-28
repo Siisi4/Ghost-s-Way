87b4f1defce4032874d0943e077523635ec6834a
Simge Erdil <siisi4officiel@gmail.com>
2020-09-28 15:28:11 +0200

    anims

---
+++ Assets/Scripts/SoundHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHolder : MonoBehaviour
+{
+    private static SoundHolder son;
+    
+    private void Awake()
+    {
+        if(son != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            son = this;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+
+    }
+}
